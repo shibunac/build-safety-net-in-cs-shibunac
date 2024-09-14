2be1d66bdcbc990fd2b7d001b617508463e6caed
shibunac <shibunachedakal@gmail.com>
2024-09-14 18:07:56 +0530

    Update Soundex.Tests.cs

--- Soundex.Tests.cs
+++ Soundex.Tests.cs
@@ -2,50 +2,66 @@ using Xunit;
 
 public class SoundexTests
 {
-    public static string TestCaseName;
-    public static string Name;
-    public static string SoundexCode;
 
-    [Fact]
-    public static Test(string testcaseName, string inputName, string soundexCode)
-    {
-        TestCaseName = testcaseName;
-        Name = inputName;
-        SoundexCode = soundexCode;
-    }
-
-    [Fact]
-    public static TestCaseScenario()
-    {
-        var testCaseList = new List<Test>
-        {
-            new Test("Testing - Empty String" , "", ""),
-            new Test("Testing - Single Character", "A", "A000"),
-            new Test("Testing - Multiple Characters", "TOM", "T500"),
-            new Test("Testing - Numbers", "1234", "1000"),
-            new Test("Testing - Special Characters", "$#@&", "$000")
-        };
-
-        foreach(var test in Test)
-        {
-            RunTests(test);
-        }
-    }
-
-    [Fact]
-    public static RunTests(Test)
-    {
-        var result = Soundex.GenerateSoundex(Test.Name);
-        Assert.Equal(result, Test.SoundexCode);
-        if(result == Test.SoundexCode)
-        {
-            Console.WriteLine("{Test.TestCaseName} : Success");
-        }
-        else
-        {
-            console.WriteLine("{Test.TestCaseName} : Failed, Expected: {Test.SoundexCode}");
-        }
-    }
+  [Fact]
+  public static void GenerateSoundex_ShouldReturnEmptyString_ForEmptyInput()
+  {
+    var newTest = new Test("Testing - Empty String", "", "");
+    var result = Soundex.GenerateSoundex(Test.Name);
+    Assert.Equal(result, Test.SoundexCode);
     
-   
+
+  }
+
+  [Fact]
+  public static void GenerateSoundex_ShouldReturnPaddedZeroes_ForSingleCharacter()
+  {
+    var newTest = new Test("Testing - Single Character", "A", "A000");
+    var result = Soundex.GenerateSoundex(Test.Name);
+    Assert.Equal(result, Test.SoundexCode);
+
+
+  }
+
+  [Fact]
+  public static void GenerateSoundex_ShouldReturnMappedCode_ForAlphabeticCharacter()
+  {
+    var newTest = new Test("Testing - Multiple Alphabet Characters", "TOM", "T500");
+    var result = Soundex.GenerateSoundex(Test.Name);
+    Assert.Equal(result, Test.SoundexCode);
+
+
+  }
+  [Fact]
+  public static void GenerateSoundex_ShouldReturnZeroesExceptFirst_ForNumbers()
+  {
+    var newTest = new Test("Testing - Numbers", "1234", "1000");
+    var result = Soundex.GenerateSoundex(Test.Name);
+    Assert.Equal(result, Test.SoundexCode);
+
+
+  }
+  [Fact]
+  public static void GenerateSoundex_ShouldReturnZeroesExceptFirst_ForSpecialCharacters()
+  {
+    var newTest = new Test("Testing - Special Characters", "$#@&", "$000");
+    var result = Soundex.GenerateSoundex(Test.Name);
+    Assert.Equal(result, Test.SoundexCode);
+
+
+  }
+}
+
+internal class Test
+{
+  public static string TestCaseName;
+  public static string Name;
+  public static string SoundexCode;
+
+  public Test(string testcaseName, string inputName, string soundexCode)
+  {
+    TestCaseName = testcaseName;
+    Name = inputName;
+    SoundexCode = soundexCode;
+  }
 }
